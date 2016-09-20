915bbe9b5c163032029609b99009f8e1d92dcba1
davidzubi93 <davidzubi93@gmail.com>
2016-09-20 07:51:50 +0000

    konexioak aldatu

--- login.php
+++ login.php
@@ -1,26 +1,22 @@
 <?php
-include(conexion.php);
-session_start();
+include "conexion.php";
+
 $usuario=$_POST['usuario'];
 $password=$_POST['password'];
 
 //select usuario
 $queryusuario = "SELECT * FROM usuario where nombre_usu='$usuario' and password='$password'";
-$resultadousu= mysql_query($queryusuario);
+$resultadousu= $mysqli->query($queryusuario);
 
-//select tarea
-$querytarea="SELECT * FROM lista";
-$resultadota=mysql_query($querytarea);
 
-if (mysqli_num_rows > 0){
-    //lerro bakoitzeko edukia bistaratzeko
-    while ($lerroa = mysql_fetch_assoc($resultadousu)){
-        echo "id: " .$lerroa["lista_id"]. " - Usuario: " .$lerroa["nombre_usu"]. "<br>";
+
+
+while ($lerroa = mysql_fetch_array($resultadousu)){
+        echo  $lerroa["nombre_usu"]."";
     }
-}else{
-    echo "0 result";
-}
-mysqli_close($conex);
+
+
+
 
 
 ?>
