b018fe401f6d747c29969916685eae738d057b91
davidzubi93 <davidzubi93@gmail.com>
2016-09-21 06:44:34 +0000

    todoapp.sql fitxategin aldaketa

--- todoapp.sql
+++ todoapp.sql
@@ -16,27 +16,54 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `lista`
+-- Table structure for table `listas`
 --
 
-DROP TABLE IF EXISTS `lista`;
+DROP TABLE IF EXISTS `listas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lista` (
-  `lista_id` int(11) NOT NULL,
-  `nombre_producto` varchar(30) DEFAULT NULL,
-  `cogido` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`lista_id`)
+CREATE TABLE `listas` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lista`
+-- Dumping data for table `listas`
 --
 
-LOCK TABLES `lista` WRITE;
-/*!40000 ALTER TABLE `lista` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lista` ENABLE KEYS */;
+LOCK TABLES `listas` WRITE;
+/*!40000 ALTER TABLE `listas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `listas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tareas`
+--
+
+DROP TABLE IF EXISTS `tareas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tareas` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(25) NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `hecho` tinyint(1) DEFAULT '0',
+  `ID_listas` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_listas` (`ID_listas`),
+  CONSTRAINT `tareas_listas` FOREIGN KEY (`ID_listas`) REFERENCES `listas` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tareas`
+--
+
+LOCK TABLES `tareas` WRITE;
+/*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -47,11 +74,14 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuarios` (
-  `id_usuarios` int(11) NOT NULL,
-  `nombre_usu` varchar(15) DEFAULT NULL,
-  `password` varchar(25) DEFAULT NULL,
-  `tipo_usu` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id_usuarios`)
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `apellido` varchar(30) DEFAULT NULL,
+  `telefono` varchar(25) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -61,8 +91,35 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'patxi','patxi','normal');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios_listas`
+--
+
+DROP TABLE IF EXISTS `usuarios_listas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios_listas` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_usuarios` int(11) NOT NULL,
+  `ID_listas` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_usuarios` (`ID_usuarios`),
+  KEY `ID_listas` (`ID_listas`),
+  CONSTRAINT `usuarios_listas_1` FOREIGN KEY (`ID_usuarios`) REFERENCES `usuarios` (`ID`),
+  CONSTRAINT `usuarios_listas_2` FOREIGN KEY (`ID_listas`) REFERENCES `listas` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios_listas`
+--
+
+LOCK TABLES `usuarios_listas` WRITE;
+/*!40000 ALTER TABLE `usuarios_listas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios_listas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -74,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-16 11:33:14
+-- Dump completed on 2016-09-21  6:43:49
