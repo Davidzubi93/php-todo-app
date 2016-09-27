1c3b9125c2534a6e7a08b3f44d8c52250dac60b5
davidzubi93 <davidzubi93@gmail.com>
2016-09-27 07:23:16 +0000

    descripcion ere pantailan bistaratzeko

--- select_tareas.php
+++ select_tareas.php
@@ -5,10 +5,10 @@ try {
     $listaId = filter_input(INPUT_GET, 'listaId', FILTER_VALIDATE_INT);
     
     if(empty($listaId)) {
-        $listas_tareas = $dbh->prepare('SELECT listas.ID as listaID, listas.nombre as listaNombre, tareas.ID as tareasID, tareas.nombre as tareasNombre
+        $listas_tareas = $dbh->prepare('SELECT listas.ID as listaID, listas.nombre as listaNombre, tareas.ID as tareasID, tareas.nombre as tareasNombre,tareas.descripcion as tareasDescripcion
             FROM listas, tareas WHERE listas.ID = tareas.ID_listas');
     } else {
-        $listas_tareas = $dbh->prepare('SELECT listas.ID as listaID, listas.nombre as listaNombre, tareas.ID as tareasID, tareas.nombre as tareasNombre
+        $listas_tareas = $dbh->prepare('SELECT listas.ID as listaID, listas.nombre as listaNombre, tareas.ID as tareasID, tareas.nombre as tareasNombre, tareas.descripcion as tareasDescripcion
             FROM listas, tareas WHERE listas.ID = tareas.ID_listas AND listas.ID = :listaId');
         
         $listas_tareas->bindParam(':listaId', $listaId);    
@@ -18,7 +18,7 @@ try {
     $resultado= $listas_tareas->fetchAll(PDO::FETCH_ASSOC);
     
     foreach($resultado as $tareas) {
-        print $tareas['tareasID'] . "-" .$tareas['tareasNombre']. "<br/>";
+        print $tareas['tareasID'] . "-" .$tareas['tareasNombre']. "-" .$tareas['tareasDescripcion']."<br/>";
     }
 }catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
