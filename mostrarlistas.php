097cb96b375990fb5d61ef302603dbae8afa74b6
davidzubi93 <davidzubi93@gmail.com>
2016-09-27 06:34:57 +0000

    FETCH_ASSOCen errorea konpondu

--- mostrarlistas.php
+++ mostrarlistas.php
@@ -6,9 +6,7 @@ try {
     $sth = $dbh->prepare("SELECT * from listas;");
     $sth->execute();
      
-    $result = $sth->fetchAll(PDO::fetch_assoc); 
-    print_r($result);
-        
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC); 
     
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
