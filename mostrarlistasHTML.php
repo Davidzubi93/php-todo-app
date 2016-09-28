1720a2c3a7e0e4264c92ee82e488ee59558bd6ac
davidzubi93 <davidzubi93@gmail.com>
2016-09-28 06:21:55 +0000

    selecteko edukia taula batean bistaratu

--- mostrarlistasHTML.php
+++ mostrarlistasHTML.php
@@ -2,7 +2,16 @@
 require_once("mostrarlistas.php");
 
 //mostrar todas las listas
+ print "<table border='1'>";
+    print "<tr>";
+    print "<th>ID</th>";
+    print "<th>Nombre</th>";
+    print "</tr>";
 foreach ($result as $resultado){
-    print $resultado['ID']."-" . $resultado['nombre'] . "<br/>";
+    print "<tr>";
+    print "<td>".$resultado['ID']."</td>";
+    print "<td>".$resultado['nombre']."</td>";
+    print "</tr>";
 }
+print "</table>";
 ?>
