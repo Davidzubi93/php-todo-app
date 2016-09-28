ecfb21c1041e738b69b965cabfa306e9ef82790a
davidzubi93 <davidzubi93@gmail.com>
2016-09-28 06:27:05 +0000

    selectean hartutakoa taula batean bistaratu

--- select_tareas.php
+++ select_tareas.php
@@ -17,9 +17,20 @@ try {
     $listas_tareas->execute();
     $resultado= $listas_tareas->fetchAll(PDO::FETCH_ASSOC);
     
+    print "<table border='1'>";
+    print "<tr>";
+    print "<th>tareasID</th>";
+    print "<th>tareasNombre</th>";
+    print "<th>tareasDescripcion</th>";
+    print "</tr>";
     foreach($resultado as $tareas) {
-        print $tareas['tareasID'] . "-" .$tareas['tareasNombre']. "-" .$tareas['tareasDescripcion']."<br/>";
+        print "<tr>";
+        print "<td>" .$tareas['tareasID'] . "</td>";
+        print "<td>" .$tareas['tareasNombre'] . "</td>";
+        print "<td>" .$tareas['tareasDescripcion'] . "</td>";
+        print "</tr>";
     }
+    print "</table>";
 }catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
