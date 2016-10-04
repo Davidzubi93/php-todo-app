faa91d3409a7b85f6713eb93786389ccf8acd8a3
davidzubi93 <davidzubi93@gmail.com>
2016-10-04 07:34:04 +0000

    while bat gehitu eta session_start jarri

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <?php
 include ("conexion.php");
 $usuario=$_POST['usuario'];
-
-$select_usuarios = "SELECT password from usuarios WHERE username=$usuario";
-$datos_select=mysql_query($select_usuarios);
+$password=$_POST['password'];
+$select_usuarios = "SELECT password from usuarios WHERE username='".$usuario."' and password='".$password."'";
+$datos_select=mysql_query($select_usuarios,$db);
 
 session_start();
 $_SESSION['usuario']=$usuario;
 
-while($row=mysql_fetch_row($datos_select)){
-    $pass=$row[0];}
-if($_POST['password']==$pass){
+/*while($row=mysql_fetch_row($datos_select)){
+    $pass=$row[0];}*/
+if($mysql_num_rows($datos_select)!=0){
     header("location:mostrarlistasHTML.php");
 }else{
     header("location:login.html");}
