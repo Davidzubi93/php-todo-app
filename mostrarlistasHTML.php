d2f352ade9e0d306cfa0fab1184a91cb8bc45fad
davidzubi93 <davidzubi93@gmail.com>
2016-10-06 09:20:47 +0000

    eremu berri bat gehitu esteka batzuekin

--- mostrarlistasHTML.php
+++ mostrarlistasHTML.php
@@ -6,11 +6,13 @@ require_once("mostrarlistas.php");
     print "<tr>";
     print "<th>ID</th>";
     print "<th>Nombre</th>";
+    print "<th>Acciones</th>";
     print "</tr>";
 foreach ($result as $resultado){
     print "<tr>";
     print "<td>".$resultado['ID']."</td>";
     print "<td>".$resultado['nombre']."</td>";
+    print "<td><a href='select_tareas.php'>Mostrar contenido</a><br/><a href='addlistas.php'>Add lista</a></td>";
     print "</tr>";
 }
 print "</table>";
