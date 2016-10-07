9f4b7822308e24241e737b0c14ffe751c8b5d43e
davidzubi93 <davidzubi93@gmail.com>
2016-10-07 09:16:06 +0000

    tareak gehitzeko formularioa

---
+++ addtareahtml.php
@@ -0,0 +1,10 @@
+<?php
+//require_once(addtarea.php);
+
+print "<form action='addtarea.php' method='POST'>";
+print "Nombre:<br/> <input type='text' name='nom'/><br/>";
+print "Descripcion:<br/> <input type='text' name='descrip'/><br/>";
+print "<input type='submit' name='Enviar' value='Enviar'/>";
+print "</form>";
+
+?>
